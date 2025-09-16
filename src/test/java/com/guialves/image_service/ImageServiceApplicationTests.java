ef37204db4798246ed13a0c4490ce79b80ae8461
guialveess <97guilherme.alves@gmail.com>
2025-09-16 14:42:24 -0300

    test: add unit and integration test structure

---
+++ src/test/java/com/guialves/image_service/ImageServiceApplicationTests.java
@@ -0,0 +1,28 @@
+package com.guialves.image_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+    @Test
+    void getAllImages() {
+    }
+
+    @Test
+    void saveImage() {
+    }
+
+    @Test
+    void deleteImageid() {
+    }
+
+    @Test
+    void getImageById() {
+    }
+}
